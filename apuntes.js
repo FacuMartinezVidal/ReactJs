5457ec2cacd1f9ebaba716c349ef60aa689393a1
Facundo Martinez Vidal <facumartinezvidal@gmail.com>
2022-11-19 20:42:34 -0300

    update apuntes

---
+++ apuntes.js
@@ -0,0 +1,37 @@
+//Sugar Syntax--> es un termino adoptado en programacion, que nos provee una funcionalidad en donde el codigo se escribe de manera mas concisa, mas facil de leer y mas rapido de escrbir.
+
+// Parse--> es una analizador de codigo
+
+//condicionales ternarios--> una manera de escribir los condicionales mas corta y distinta
+
+// condicion ? resultadoTrue : resultadoFalse;
+
+// forof--> sintaxix sugar del for, sirve para trabajar con objetos iteralbles
+// for (const itemList of lista) {
+//   console.log(iterator);
+// }
+
+// const lista = ["auto", "moto", "colectivo", "camion"];
+
+// for (let index = 0; index < lista.length; index++) {
+//   console.log(lista[index]);
+// }
+
+//declaracion--> no devuelve un resultado de manera inmediata, sino que nos permite armar un cuerpo y armar un monton de resultados como es el caso de if/else
+//expresion--> devuelve un resultado indmediato, como es el caso del condicional ternario
+
+//polyfills--> es un script que actualiza o agrega una nueva funcion al lenguaje, nos ayuda a tener codigo retrocompatible posible de ser usado en navegadores y motores de js antiguo
+// const truncApplied = Math.trunc(1.23); // nos devuelve solo el numero entero
+
+//operador nullish--coalescing si la variable es null o undefined nos cambia el valor a 100 (en este caso)
+// let height;
+// height = height ?? 100;
+
+//transpilers--> traduce una pieza de codigo fuente a otro codigo fuente
+
+//find--> nos devuelve el primer valor que encuentra apartir de la condicion que le establecemos
+
+//react--> se utilizan componentes, es decir la aplicacion va a ser un conjunto de componentes
+//componentes--> antes, los componentes se creaban apartir de clases, pero ahora se crea apartir de funciones
+//componentes-->pueden recibir propiedades y tienen la capacidad de hacer render de un unico elemento
+//componentes--> los nombres de los componentes en react, su primer letra va en mayuscula
